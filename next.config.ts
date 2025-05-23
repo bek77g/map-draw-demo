c5c24759b5178c5d2649a12fabe2054b7835ced4
Beknur <d.zheenkul@gmail.com>
2025-05-23 15:28:28 +0600

    chore: disable GitHub Pages deployment config and settings

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from 'next';
 
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'map-draw-demo';
+// const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'map-draw-demo';
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
-	images: {
-		unoptimized: true,
-	},
+	// output: 'export',
+	// basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+	// images: {
+	// 	unoptimized: true,
+	// },
 };
 
 export default nextConfig;
