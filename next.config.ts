de7397ee448f80ee252ec9bce8b50e5fd5c2bf71
Beknur <d.zheenkul@gmail.com>
2025-05-23 15:27:00 +0600

    feat: configure Next.js for GitHub Pages deployment with base path and workflow updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'map-draw-demo';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'export',
+	basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
